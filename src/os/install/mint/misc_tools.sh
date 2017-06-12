af5f3539ec86102763010b8e3dc892c36cc7a161
Fred Lackey <fred.lackey@gmail.com>
2017-06-12 19:17:50 -0400

    add other oses

--- src/os/install/mint/misc_tools.sh
+++ src/os/install/mint/misc_tools.sh
@@ -16,26 +16,26 @@ install_package "JQ" "jq"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# if ! package_is_installed "powermate"; then
-#
-#     add_ppa "stefansundin/powermate" \
-#         || print_error "Powermate (add PPA)"
-#
-#     # update &> /dev/null \
-#     #     || print_error "Powermate (resync package index files)" \
-#
-#     execute \
-#         "sudo apt-get update \
-#             && sudo apt-get install -f" \
-#         "Powermate (resync package index files)"
-#
-#     # execute \
-#     #   "wget -q -O /tmp/powermate.deb https://github.com/stefansundin/powermate.deb \
-#     #       && sudo dpkg -i /tmp/powermate.deb \
-#     #       && sudo apt-get install -f" \
-#     #   "Powermate"
-#
-# fi
+if ! package_is_installed "powermate"; then
+
+    add_ppa "stefansundin/powermate" \
+        || print_error "Powermate (add PPA)"
+
+    # update &> /dev/null \
+    #     || print_error "Powermate (resync package index files)" \
+
+    execute \
+        "sudo apt-get update \
+            && sudo apt-get install -f" \
+        "Powermate (update sources)"
+
+    # execute \
+    #   "wget -q -O /tmp/powermate.deb https://github.com/stefansundin/powermate.deb \
+    #       && sudo dpkg -i /tmp/powermate.deb \
+    #       && sudo apt-get install -f" \
+    #   "Powermate"
+
+fi
 install_package "Powermate" "powermate"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
