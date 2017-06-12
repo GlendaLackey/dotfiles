221e17d650ceeb19b7d1e9013948880c9450405f
Fred Lackey <fred.lackey@gmail.com>
2017-06-12 15:48:06 -0400

    add other oses

--- src/os/install/ubuntu/development.sh
+++ src/os/install/ubuntu/development.sh
@@ -94,15 +94,13 @@ if ! package_is_installed "javac"; then
         "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections" \
         "Oracle Java 8 (set defaults)"
     execute \
-        "sudo apt-get update \
-            && apt-get install oracle-java8-installer" \
-        "Oracle Java 8"
+        "sudo apt-get update" \
+        "Oracle Java 8 (sync pakages)"
+    install_package "Oracle Java 8" "oracle-java8-installer"
     execute \
         "sudo update-java-alternatives -s java-8-oracle" \
         "Oracle Java 8 (add to alternates)"
-    execute \
-        "sudo apt-get install oracle-java8-set-default" \
-        "Oracle Java 8 (set default)"
+    install_package "Oracle Java 8 (set default)" "oracle-java8-set-default"
 fi
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
