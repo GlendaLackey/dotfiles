9186a4a469ed87f1425bca82a40868cce21ea4d9
Fred Lackey <fred.lackey@gmail.com>
2017-06-14 09:49:16 -0400

    add other oses

--- src/os/install/macos/productivity.sh
+++ src/os/install/macos/productivity.sh
@@ -10,6 +10,7 @@ print_in_purple "\n   Productivity\n\n"
 
 brew_install "Evernote" "evernote" "caskroom/cask" "cask"
 brew_install "LibreOffice" "libreoffice" "caskroom/cask" "cask"
+brew_install "Grammarly" "grammarly" "caskroom/cask" "cask"
 brew_install "Macdown" "macdown" "caskroom/cask" "cask"
 brew_install "Microsoft Office" "microsoft-office" "caskroom/cask" "cask"
 brew_install "Sublime Text" "sublime-text" "caskroom/cask" "cask"
